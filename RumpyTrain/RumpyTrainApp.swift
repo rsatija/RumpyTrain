fac9b900160ac5642268c8db95c65cf404723fb3
Rahul Satija <satija.rahul@gmail.com>
2025-03-05 21:29:01 -0500

    Initial Commit

---
+++ RumpyTrain/RumpyTrainApp.swift
@@ -0,0 +1,17 @@
+//
+//  RumpyTrainApp.swift
+//  RumpyTrain
+//
+//  Created by Rahul Satija on 3/5/25.
+//
+
+import SwiftUI
+
+@main
+struct RumpyTrainApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
